00a86daa3aa3fcdc6ba658f7a7007494c922aaba
felipe.apires10 <felipe.apires10@gmail.com>
2025-11-28 22:59:10 +0000

    start project

---
+++ cardapio.py
@@ -0,0 +1,18 @@
+def get_cardapio():
+    cardapio = [
+        {
+            'id': 1,
+            'titulo': 'Bolo de chocolate',
+            'descricao': 'Delicioso e chocolatudo bolo de chocolate',
+            'foto': ''
+        },
+        {
+            'id': 2,
+            'titulo': 'Bolo de limão',
+            'descricao': 'Delicioso e cítrico bolo de limão',
+            'foto': ''
+        }
+
+
+    ]
+    return cardapio
