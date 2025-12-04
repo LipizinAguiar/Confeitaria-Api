2ebe75366acb4fcbdcef33feb8e836ed29cf5d0e
Felipe Aguiar <felipe.aguiarpires@hotmail.com>
2025-12-04 19:23:01 -0300

    JJ

--- conexao.py
+++ conexao.py
@@ -8,7 +8,7 @@ def get_conexao():
     conn = psycopg2.connect(
         dbname=os.getenv('DB_DATABASE'),
         user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSOWRD'),
+        password=os.getenv('DB_PASSWORD'),
         host=os.getenv('DB_HOST'),
         port=os.getenv('DB_PORT')
     )
