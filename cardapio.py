fc751dbecdfe86b5c87ab85bf0d3e85b3581141f
Felipe Aguiar <felipe.aguiarpires@hotmail.com>
2025-12-04 19:42:07 -0300

    yjyjyj

--- cardapio.py
+++ cardapio.py
@@ -3,12 +3,11 @@ from psycopg2.extras import RealDictCursor
 from flask import jsonify
 
 
-
 def get_cardapio():
-   conn = get_conexao()
-   cursor = conn.cursor(cursor_factory = RealDictCursor)
-   cursor.execute('SELECT * FROM Confeitaria')
-   cardapio = cursor.fetchall()
-   cursor.close()
-   conn.close()
-   return jsonify(cardapio)
+    conn = get_conexao()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute('SELECT * FROM cardapio')
+    cardapio = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(cardapio)
